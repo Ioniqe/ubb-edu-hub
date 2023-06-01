87f53b4500902ce54dfbc5b23f7162d2b9cfc792
Ioana Bozdog <ioana.bozdog@linnify.com>
2023-06-01 14:19:17 +0300

    feat(not found): add styling to not found

--- apps/host/src/pages/NotFound.tsx
+++ apps/host/src/pages/NotFound.tsx
@@ -4,5 +4,19 @@ import { useAppTheme } from "ui";
 
 export const NotFound = () => {
   const { theme } = useAppTheme();
-  return <Box>Not found</Box>;
+  return (
+    <Box
+      display={"flex"}
+      width={"100vw"}
+      height={"100vh"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.text.secondary,
+      }}
+    >
+      <Typography variant={"h1"}>Page not found</Typography>
+    </Box>
+  );
 };
