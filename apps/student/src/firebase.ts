6affcc44faa3308260c097571296056619c3d980
Ioana Bozdog <ioana.bozdog@linnify.com>
2023-06-05 12:36:03 +0300

    feat(badges): add firebase storage and fetch badges from there

---
+++ apps/student/src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Cjzd3R3OzPGY1jT_HczdFY8ZUQslHLE",
+  authDomain: "ubb-edu-hub.firebaseapp.com",
+  projectId: "ubb-edu-hub",
+  storageBucket: "ubb-edu-hub.appspot.com",
+  messagingSenderId: "204446372637",
+  appId: "1:204446372637:web:f26404daba935957c547f4",
+  measurementId: "G-4Z1L0P4YX3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
--- apps/student/src/pages/Badges.tsx
+++ apps/student/src/pages/Badges.tsx
@@ -1,8 +1,69 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, CircularProgress } from "@mui/material";
+import { CustomAppThemeProvider } from "ui/CustomAppThemeProvider";
+import {
+  getDownloadURL,
+  listAll,
+  ref,
+  StorageReference,
+} from "firebase/storage";
+import { storage } from "../firebase";
+import { FirebaseImage } from "../types";
 
 const Badges = () => {
-  return <Box>The Badges for the Student.</Box>;
+  const [loading, setLoading] = useState(true);
+  const imagesListRef = ref(storage, "/badges");
+  const [imageUrls, setImageUrls] = useState<FirebaseImage[]>([]);
+
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item: StorageReference) => {
+        getDownloadURL(item).then((url) => {
+          const newImage: FirebaseImage = {
+            name: item.name,
+            url,
+          };
+
+          setImageUrls((prev: FirebaseImage[]) => [...prev, newImage]);
+        });
+      });
+    });
+
+    // TODO fetch from back
+    setLoading(false);
+  }, []);
+
+  return (
+    <CustomAppThemeProvider>
+      {loading ? (
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          width={"100%"}
+          height={"100%"}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"}>
+          {imageUrls.map((image: FirebaseImage, index) => (
+            <Box
+              key={index}
+              component={"img"}
+              alt={image.name}
+              src={image.url}
+              width={"136px"}
+              height={"136px"}
+              sx={{
+                m: 2,
+              }}
+            />
+          ))}
+        </Box>
+      )}
+    </CustomAppThemeProvider>
+  );
 };
 
 export default Badges;
